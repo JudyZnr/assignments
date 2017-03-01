75dc17bff5f89c04f674b13363e2894b39a31a6e
JudyZnr <JudyZnr@gmail.com>
2017-03-01 15:39:01 -0500

    Lab3 is done
    simplify the function of calculating the means and variance
    each feature associate with a variance
    for each class, var should be initialize as 0 at first
    only calculate predict for once

--- Assignment3.py
+++ Assignment3.py
@@ -8,7 +8,7 @@ Created on Wed Feb 22 15:46:10 2017
 
 import numpy as np
 import pandas as pd
-import math
+
 
 class GaussianNB():
     
@@ -16,287 +16,98 @@ class GaussianNB():
     
     def __init__(self):
         self.offset = 10e-5
+        self.p_x_yi = 0
                
-    def accurateScore(y1,y2):
+    def accurateScore(self,y1,y2):
         score = y1==y2
         return np.average(score)
     
-    def confusion_matrix(y1,y2):
-        return pd.crosstab(y_true,y_pred,
-                           rownames=['Actual'],columnames=['Predicted'])
+    def confusion_matrix(self,y1,y2):
+        return pd.crosstab(y1,y2,
+                           rownames=['Actual'],colnames=['Predicted'])
     
-    def computeMeans(self, N, X, y):
+    def computeMeans(self, X, y):
 # mean value of each feature per class(conditional)
-        y_n = y
-        self.sum_mean_0 = np.zeros(self.n_features)
-        self.sum_mean_1 = np.zeros(self.n_features)
-        self.sum_mean_2 = np.zeros(self.n_features)
-        self.sum_mean_3 = np.zeros(self.n_features)
-        self.sum_mean_4 = np.zeros(self.n_features)
-        self.sum_mean_5 = np.zeros(self.n_features)
-        self.sum_mean_6 = np.zeros(self.n_features)
-        self.sum_mean_7 = np.zeros(self.n_features)
-        self.sum_mean_8 = np.zeros(self.n_features)
-        self.sum_mean_9 = np.zeros(self.n_features)
+        num_classes = len(np.unique(y))
+        self.sum_mean = np.zeros((num_classes, self.n_features))
+        self.sum_var = np.zeros(self.n_features)
         
-        for i in range(self.n_instances):
-            if y_n[i] == 0:
-                x_n = X[i,:]
-                self.function_0(self,x_n)
-            elif y_n[i] == 1:
-                x_n = X[i,:]
-                self.function_1(self,x_n)
-            elif y_n[i] == 2:
-                x_n = X[i,:]
-                self.function_2(self,x_n)
-            elif y_n[i] == 3:
-                x_n = X[i,:]
-                self.function_3(self,x_n)
-            elif y_n[i] == 4:
-                x_n = X[i,:]
-                self.function_4(self,x_n)
-            elif y_n[i] == 5:
-                x_n = X[i,:]
-                self.function_5(self,x_n)
-            elif y_n[i] == 6:
-                x_n = X[i,:]
-                self.function_6(self,x_n)
-            elif y_n[i] == 7:
-                x_n = X[i,:]
-                self.function_7(self,x_n)
-            elif y_n[i] == 8:
-                x_n = X[i,:]
-                self.function_8(self,x_n)
-            elif y_n[i] == 9:
-                x_n = X[i,:]
-                self.function_9(self,x_n)
-                
-    def function_0(self, x):
-        for j in range(self.n_features):
-            self.sum_mean_0[j] = self.sum_mean_0[j] + x[j]
-        self.meansArray_0 = np.zeros(self.n_features)
-        self.meansArray_0 = 1/self.n_instances * sum_mean_0
-    
-    def function_1(self, x):
-        for j in range(self.n_features):
-            self.sum_mean_1[j] = self.sum_mean_1[j] + x[j]
-        self.meansArray_1 = np.zeros(self.n_features)
-        self.meansArray_1 = 1/self.n_instances * sum_mean_1
-    
-    def function_2(self, x):
-        for j in range(self.n_features):
-            self.sum_mean_2[j] = self.sum_mean_2[j] + x[j]
-        self.meansArray_2 = np.zeros(self.n_features)
-        self.meansArray_2 = 1/self.n_instances * sum_mean_2
-    
-    def function_3(self, x):
-        for j in range(self.n_features):
-            self.sum_mean_3[j] = self.sum_mean_3[j] + x[j]
-        self.meansArray_3 = np.zeros(self.n_features)
-        self.meansArray_3 = 1/self.n_instances * sum_mean_3
-    
-    def function_4(self, x):
-        for j in range(self.n_features):
-            self.sum_mean_4[j] = self.sum_mean_4[j] + x[j]
-        self.meansArray_4 = np.zeros(self.n_features)
-        self.meansArray_4 = 1/self.n_instances * sum_mean_4
-    
-    def function_5(self, x):
-        for j in range(self.n_features):
-            self.sum_mean_6[j] = self.sum_mean_6[j] + x[j]
-        self.meansArray_5 = np.zeros(self.n_features)
-        self.meansArray_5 = 1/self.n_instances * sum_mean_5
-    
-    def function_6(self, x):
-        for j in range(self.n_features):
-            self.sum_mean_6[j] = self.sum_mean_6[j] + x[j]
-        self.meansArray_6 = np.zeros(self.n_features)
-        self.meansArray_6 = 1/self.n_instances * sum_mean_6
-    
-    def function_7(self, x):
-        for j in range(self.n_features):
-            self.sum_mean_7[j] = self.sum_mean_7[j] + x[j]
-        self.meansArray_7 = np.zeros(self.n_features)
-        self.meansArray_7 = 1/self.n_instances * sum_mean_7
-    
-    def function_8(self, x):
-        for j in range(self.n_features):
-            self.sum_mean_8[j] = self.sum_mean_8[j] + x[j]
-        self.meansArray_8 = np.zeros(self.n_features)
-        self.meansArray_8 = 1/self.n_instances * sum_mean_8
-    
-    def function_9(self, x):
-        for j in range(self.n_features):
-            self.sum_mean_9[j] = self.sum_mean_9[j] + x[j]
-        self.meansArray_9 = np.zeros(self.n_features)
-        self.meansArray_9 = 1/self.n_instances * sum_mean_9
-        
-        
-        
-    def computeVariance(self, N, X, y):        
-    # variance value of each class plus offset
-        y_n = y
-        for i in range(self.n_instances):
-            if y_n[i] == 0:
-                x_n = X[i,:]
-                for j in range(self.n_features):
-                    sum_0[j] = sum_0[j] + math.pow((x_n[j] - self.meansArray_0[j]),2)
-                
-            elif y_n[i] == 1:
-                x_n = X[i,:]
-                for j in range(self.n_features):
-                    sum_1[j] = sum_1[j] + math.pow((x_n[j] - self.meansArray_1[j]),2)
-                    
-            elif y_n[i] == 2:
-                x_n = X[i,:]
-                for j in range(self.n_features):
-                    sum_2[j] = sum_2[j] + math.pow((x_n[j] - self.meansArray_2[j]),2)
-                    
-            elif y_n[i] == 3:
-                x_n = X[i,:]
-                for j in range(self.n_features):
-                    sum_3[j] = sum_3[j] + math.pow((x_n[j] - self.meansArray_0[j]),2)
-                    
-            elif y_n[i] == 4:
-                x_n = X[i,:]
-                for j in range(self.n_features):
-                    sum_4[j] = sum_4[j] + math.pow((x_n[j] - self.meansArray_4[j]),2)
-                    
-            elif y_n[i] == 5:
-                x_n = X[i,:]
-                for j in range(self.n_features):
-                    sum_5[j] = sum_5[j] + math.pow((x_n[j] - self.meansArray_5[j]),2)
-                    
-            elif y_n[i] == 6:
-                x_n = X[i,:]
-                for j in range(self.n_features):
-                    sum_6[j] = sum_6[j] + math.pow((x_n[j] - self.meansArray_6[j]),2)
-                    
-            elif y_n[i] == 7:
-                x_n = X[i,:]
-                for j in range(self.n_features):
-                    sum_7[j] = sum_7[j] + math.pow((x_n[j] - self.meansArray_7[j]),2)
-                    
-            elif y_n[i] == 8:
-                x_n = X[i,:]
-                for j in range(self.n_features):
-                    sum_8[j] = sum_8[j] + math.pow((x_n[j] - self.meansArray_8[j]),2)
-                    
-            elif y_n[i] == 9:
-                x_n = X[i,:]
-                for j in range(self.n_features):
-                    sum_9[j] = sum_9[j] + math.pow((x_n[j] - self.meansArray_9[j]),2)
-        
-        self.Var_0 = 1/(self.n_instances - 1) * sum_0
-        self.Var_1 = 1/(self.n_instances - 1) * sum_1
-        self.Var_2 = 1/(self.n_instances - 1) * sum_2
-        self.Var_3 = 1/(self.n_instances - 1) * sum_3
-        self.Var_4 = 1/(self.n_instances - 1) * sum_4
-        self.Var_5 = 1/(self.n_instances - 1) * sum_5
-        self.Var_6 = 1/(self.n_instances - 1) * sum_6
-        self.Var_7 = 1/(self.n_instances - 1) * sum_7
-        self.Var_8 = 1/(self.n_instances - 1) * sum_8
-        self.Var_9 = 1/(self.n_instances - 1) * sum_9
-        
-        self.varc = np.array([self.Var_0,self.Var_1,self.Var_2,self.Var_3,self.Var_4,
-                                  self.Var_5,self.Var_6,self.Var_7,self.Var_8,self.Var_9])
-                       
+        for y_n in range(num_classes):
+            X_i = X[y == y_n]
+            self.sum_mean[y_n] = np.mean(X_i)
+     
+    def computeVariance(self, X, y):
+        self.sum_var = np.var(X)
+        self.sum_var += self.offset
                        
     
     def prior_class(self,y):
-        y_n = y
-        count_0 = None
-        count_1 = None
-        count_2 = None
-        count_3 = None
-        count_4 = None
-        count_5 = None
-        count_6 = None
-        count_7 = None
-        count_8 = None
-        count_9 = None
-        for t in range(self.n_instances):
-            if y_n[t]==0:
-                count_0 = count_0 +1
-            elif y_n[t]==1:
-                count_1 = count_1 +1
-            elif y_n[t]==2:
-                count_1 = count_2 +1
-            elif y_n[t]==3:
-                count_1 = count_3 +1
-            elif y_n[t]==4:
-                count_1 = count_4 +1
-            elif y_n[t]==5:
-                count_1 = count_5 +1
-            elif y_n[t]==6:
-                count_1 = count_6 +1
-            elif y_n[t]==7:
-                count_1 = count_7 +1
-            elif y_n[t]==8:
-                count_1 = count_8 +1
-            elif y_n[t]==8:
-                count_1 = count_9 +1
-                
-        self.p_y0 = count_0 / self.n_instances
-        self.p_y1 = count_1 / self.n_instances
-        self.p_y2 = count_2 / self.n_instances
-        self.p_y3 = count_3 / self.n_instances
-        self.p_y4 = count_4 / self.n_instances
-        self.p_y5 = count_5 / self.n_instances
-        self.p_y6 = count_6 / self.n_instances
-        self.p_y7 = count_7 / self.n_instances
-        self.p_y8 = count_8 / self.n_instances
-        self.p_y9 = count_9 / self.n_instances
-        self.p_y = np.array([self.p_y0,self.p_y1,self.p_y2,self.p_y3,self.p_y4,
-                             self.p_y5,self.p_y6,self.p_y7,self.p_y8,self.p_y9])
-            
+        num_classes = len(np.unique(y))
+        self.p_y = np.zeros(num_classes)
+        
+        for y_n in range(num_classes):
+            self.p_y[y_n] = len(y[y == y_n]) / float(len(y))
+
     def joint_likelihood(self,x):
-        temp =None
+        temp =0
+        var1=0
+        var2=0
+        # var3 = log(P(y=class|X=instance))
+        var3=0
+        var4=0
+        var5=[]
+        self.p_yi_x = np.zeros(len(self.p_y))
         for i in range(len(self.p_y)):
+            var2 = 0
             for j in range(self.n_features):
                 x_nf = x[j]
-                temp = math.pow((x_nf - self.meansArray[i][j]),2)/2*math.pow((self.varc[i]),2)
-                p_xj_yi= (1/ np.sqrt(2*pi*self.varc[i]))* math.exp(-temp)
+                temp = np.square(x_nf - self.sum_mean[i,j]) / (2 * self.sum_var[j])
+                self.p_xj_yi= (1/ np.sqrt(2* np.pi * np.sqrt(self.sum_var[j]))) * np.exp(-temp)
                 # calculate the P(X=instance|y=class)
-                np.log(self.p_x_yi) = np.log(self.p_x_yi) + np.log(p_xj_yi)
+                var1 = np.log(self.p_xj_yi)
+                #np.log(self.p_x_yi) = np.log(self.p_x_yi) + np.log(self.p_xj_yi)
             # calculate the P(y=class|X=instance) we can get the predict value
-            np.log(self.p_yi_x[i]) = np.log(self.p_y[i]) + np.log(self.p_x_yi)
-        
-        return np.argmax(self.p_yi_x)
+                var2 += var1
+            var3 = np.log(self.p_y[i])
+            var4 = var2 + var3
+            var5.append(var4)
+            
+        #return np.argmax(self.p_yi_x)
+        return np.argmax(var5)
     
   
     def fit(self, X, y):
         
         self.n_instances,self.n_features = np.shape(X)
-        self.computeMeans(self.n_instances, X,y)
-        self.meansArray = np.array([self.meansArray_0,self.meansArray_1,self.meansArray_2,self.meansArray_3,self.meansArray_4,
-                                    self.meansArray_5,self.meansArray_6,self.meansArray_7,self.meansArray_8,self.meansArray_9])
-        self.computeVariance(self.n_instances, X,y)
+        self.computeMeans(X,y)
+        
+        self.computeVariance(X,y)
         self.prior_class(y)
        
         return self
     
     def predict(self,X,y):
-        self.y_pred = np.zeros(len(y))
+        y_pred = np.zeros(len(y))
         for i in range(self.n_instances):
-            x_n = X[i,:]
-            self.y_pred[i] = self.joint_likelihood(x_n)
+            x_n = X.ix[i,:]
+            y_pred[i] = self.joint_likelihood(x_n)
         
-if __name__=="__main__":
-    
+        return y_pred
+
+if __name__=="__main__": 
     nb = GaussianNB()
     with open("output.txt","w") as f_out:
         df = pd.read_csv('/Users/administorzz/Downloads/digits.csv')
         X = df.ix[:,:-1]
         y = df.ix[:,-1]
-        self.y_true = y
+        y_true = y
         
         nb.fit(X,y)
-        nb.predict(X)
+        y_pred = nb.predict(X,y)
         
-        f_out.write(nb.accurateScore(self.y_true,self.y_pred))
+        f_out.write("{0:.3f}".format(nb.accurateScore(y_true, y_pred)))
         f_out.write('\n')
-        f_out.write(nb.confusion_matrix(self.y_true,self.y_pred))
+        f_out.write(str(nb.confusion_matrix(y_true, y_pred)))
         
     
             
